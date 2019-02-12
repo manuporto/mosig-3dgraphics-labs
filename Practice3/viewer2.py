7bc6c452687c2c752c20e13c331f10b7e7b38b31
Manuel Porto <manu.porto94@gmail.com>
2019-02-12 23:06:00 +0100

    Add practices 2 and 3

--- Practice3/viewer2.py
+++ Practice3/viewer2.py
@@ -4,16 +4,18 @@ Python OpenGL practical application.
 """
 # Python built-in modules
 import os                           # os function, i.e. checking file status
+from itertools import cycle
+import sys
 
 # External, non built-in modules
 import OpenGL.GL as GL              # standard Python OpenGL wrapper
 import glfw                         # lean window system wrapper for OpenGL
 import numpy as np                  # all matrix manipulations & OpenGL args
-import pyassimp                     # 3D ressource loader
-import pyassimp.errors              # assimp error management + exceptions
+import pyassimp                     # 3D resource loader
+import pyassimp.errors              # Assimp error management + exceptions
 
-# Own code
-from transform import translate, rotate, scale, vec, perspective, lookat
+from transform import Trackball, identity, translate, rotate, scale
+
 
 # ------------ low level OpenGL object wrappers ----------------------------
 class Shader:
@@ -59,30 +61,8 @@ class Shader:
             GL.glDeleteProgram(self.glid)  # object dies => destroy GL object
 
 
-# ------------  Simple color shaders ------------------------------------------
-COLOR_VERT = """#version 330 core
-uniform mat4 matrix;
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
-out vec4 outPosition;
-void main() {
-    outPosition = vec4(color, 1);
-    gl_Position = matrix * vec4(position, 1);
-
-}"""
-
-COLOR_FRAG = """#version 330 core
-in vec4 outPosition;
-out vec4 outColor;
-void main() {
-    outColor = outPosition;
-
-}"""
-# outColor = vec4(color, 1);
-
-# ------------  Scene object classes ------------------------------------------
 class VertexArray:
-    """ helper class to create and self destroy OpenGL vertex array objects. """
+    """ helper class to create and self destroy OpenGL vertex array objects."""
     def __init__(self, attributes, index=None, usage=GL.GL_STATIC_DRAW):
         """ Vertex array from attributes and optional index array. Vertex
             Attributes should be list of arrays with one row per vertex. """
@@ -131,87 +111,135 @@ class VertexArray:
         GL.glDeleteVertexArrays(1, [self.glid])
         GL.glDeleteBuffers(len(self.buffers), self.buffers)
 
+
+# ------------  simple color fragment shader demonstrated in Practical 1 ------
+COLOR_VERT = """#version 330 core
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+out vec3 fragColor;
+
+void main() {
+    gl_Position = projection * view * model * vec4(position, 1);
+    fragColor = color;
+}"""
+
+
+COLOR_FRAG = """#version 330 core
+in vec3 fragColor;
+out vec4 outColor;
+void main() {
+    outColor = vec4(fragColor, 1);
+}"""
+
+
+# ------------  Scene object classes ------------------------------------------
+
+class Node:
+    """ Scene graph transform and parameter broadcast node """
+    def __init__(self, name='', children=(), transform=identity(), **param):
+        self.transform, self.param, self.name = transform, param, name
+        self.children = list(iter(children))
+
+    def add(self, *drawables):
+        """ Add drawables to this node, simply updating children list """
+        self.children.extend(drawables)
+
+    def draw(self, projection, view, model, **param):
+        """ Recursive draw, passing down named parameters & model matrix. """
+        # merge named parameters given at initialization with those given here
+        param = dict(param, **self.param)
+        # model = ...   # what to insert here for hierarchical update?
+        for child in self.children:
+            # model = model @ self.transform
+            child.draw(projection, view, model, **param)
+
+
+# mesh to refactor all previous classes
 class ColorMesh:
 
     def __init__(self, attributes, index=None):
         self.vertex_array = VertexArray(attributes, index)
 
-    def draw(self, projection, view, model, color_shader):
-       GL.glUseProgram(color_shader.glid)
-       self.vertex_array.execute(GL.GL_TRIANGLES) 
-    
+    def draw(self, projection, view, model, color_shader, **param):
+
+        names = ['view', 'projection', 'model']
+        loc = {n: GL.glGetUniformLocation(color_shader.glid, n) for n in names}
+        GL.glUseProgram(color_shader.glid)
+
+        GL.glUniformMatrix4fv(loc['view'], 1, True, view)
+        GL.glUniformMatrix4fv(loc['projection'], 1, True, projection)
+        GL.glUniformMatrix4fv(loc['model'], 1, True, model)
+
+        # draw triangle as GL_TRIANGLE vertex array, draw array call
+        self.vertex_array.execute(GL.GL_TRIANGLES)
+
+
 class SimpleTriangle(ColorMesh):
     """Hello triangle object"""
 
     def __init__(self):
-        # triangle position and color buffers
+
+        # triangle position buffer
         position = np.array(((0, .5, 0), (.5, -.5, 0), (-.5, -.5, 0)), 'f')
-        color = np.array(((1,0,0), (0,1,0), (0,0,1)), 'f')
+        color = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)), 'f')
+
         super().__init__([position, color])
 
-    def draw(self, projection, view, model, color_shader, color):
-        super().draw(projection, view, model, color_shader)
 
-        # model, projection and view transform
-        model =  np.identity(4) # translate(0.4, 0.7, 0) @ rotate(vec(1, 0, 0), 25) @ scale(0.7)
-        view = lookat(np.array((4, 3, 3), 'f'), np.array((0, 0, 0), 'f'), np.array((0, 1, 0), 'f'))
-        projection = perspective(45.0, 4/3, 0.1, 100.0)
-
-        # transformation
-        mvp = projection @ view @ model
-        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
-        GL.glUniformMatrix4fv(matrix_location, 1, True, mvp)
-
-class Pyramid(ColorMesh):
-    """Pyramid object"""
-
+class Cylinder(Node):
+    """ Very simple cylinder based on practical 2 load function """
     def __init__(self):
+        super().__init__()
+        self.add(*load('cylinder.obj'))  # just load the cylinder from file
 
-        # triangle position and color buffers
-        position = np.array(((-.5, 0, -.5), (.5, 0, -.5), (.5, 0, .5), (-.5, 0, .5), (0, 1, 0)), np.float32)
-        index = np.array((0, 3, 4, 0, 1, 4, 2, 1, 4, 3, 2, 4), np.uint32)
-        color = np.array(((1,0,0), (0,1,0), (0,0,1), (1,1,0), (0,1,1)), 'f')
-        super().__init__([position, color], index)
 
-    def draw(self, projection, view, model, color_shader, color):
-        super().draw(projection, view, model, color_shader)
+# -------------- 3D ressource loader -----------------------------------------
+def load(file):
+    """ load resources from file using pyassimp, return list of ColorMesh """
+    try:
+        option = pyassimp.postprocess.aiProcessPreset_TargetRealtime_MaxQuality
+        scene = pyassimp.load(file, option)
+    except pyassimp.errors.AssimpError:
+        print('ERROR: pyassimp unable to load', file)
+        return []  # error reading => return empty list
 
-        # model, projection and view transform
-        model =  np.identity(4) # translate(0.4, 0.7, 0) @ rotate(vec(1, 0, 0), 25) @ scale(0.7)
-        view = lookat(np.array((0, 3, 3), 'f'), np.array((0, 0, 0), 'f'), np.array((0, 1, 0), 'f'))
-        projection = perspective(45.0, 4/3, 0.1, 100.0)
+    meshes = [ColorMesh([m.vertices, m.normals], m.faces) for m in scene.meshes]
+    size = sum((mesh.faces.shape[0] for mesh in scene.meshes))
+    print('Loaded %s\t(%d meshes, %d faces)' % (file, len(scene.meshes), size))
 
-        # transformation
-        mvp = projection @ view @ model
-        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
-        GL.glUniformMatrix4fv(matrix_location, 1, True, mvp)
+    pyassimp.release(scene)
+    return meshes
 
-class Pyramid2(ColorMesh):
-    """Pyramid object"""
-
-    def __init__(self):
-
-        # triangle position and color buffers
-        position = np.array(((-.5, 0, -.5), (.5, 0, -.5), (.5, 0, .5), (-.5, 0, .5), (0, 1, 0)), np.float32)
-        position += (2, 0, 0)
-        index = np.array((0, 3, 4, 0, 1, 4, 2, 1, 4, 3, 2, 4), np.uint32)
-        color = np.array(((1,0,0), (0,1,0), (0,0,1), (1,1,0), (0,1,1)), 'f')
-        super().__init__([position, color], index)
-
-    def draw(self, projection, view, model, color_shader, color):
-        super().draw(projection, view, model, color_shader)
-
-        # model, projection and view transform
-        model =  np.identity(4) # translate(0.4, 0.7, 0) @ rotate(vec(1, 0, 0), 25) @ scale(0.7)
-        view = lookat(np.array((0, 3, 3), 'f'), np.array((0, 0, 0), 'f'), np.array((0, 1, 0), 'f'))
-        projection = perspective(45.0, 4/3, 0.1, 100.0)
-
-        # transformation
-        mvp = projection @ view @ model
-        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
-        GL.glUniformMatrix4fv(matrix_location, 1, True, mvp)
 
 # ------------  Viewer class & window management ------------------------------
+class GLFWTrackball(Trackball):
+    """ Use in Viewer for interactive viewpoint control """
+
+    def __init__(self, win):
+        """ Init needs a GLFW window handler 'win' to register callbacks """
+        super().__init__()
+        self.mouse = (0, 0)
+        glfw.set_cursor_pos_callback(win, self.on_mouse_move)
+        glfw.set_scroll_callback(win, self.on_scroll)
+
+    def on_mouse_move(self, win, xpos, ypos):
+        """ Rotate on left-click & drag, pan on right-click & drag """
+        old = self.mouse
+        self.mouse = (xpos, glfw.get_window_size(win)[1] - ypos)
+        if glfw.get_mouse_button(win, glfw.MOUSE_BUTTON_LEFT):
+            self.drag(old, self.mouse, glfw.get_window_size(win))
+        if glfw.get_mouse_button(win, glfw.MOUSE_BUTTON_RIGHT):
+            self.pan(old, self.mouse)
+
+    def on_scroll(self, win, _deltax, deltay):
+        """ Scroll controls the camera distance to trackball center """
+        self.zoom(deltay, glfw.get_window_size(win)[1])
+
+
 class Viewer:
     """ GLFW viewer window, with classic initialization & graphics loop """
 
@@ -238,6 +266,8 @@ class Viewer:
 
         # initialize GL by setting viewport and default render characteristics
         GL.glClearColor(0.1, 0.1, 0.1, 0.1)
+        GL.glEnable(GL.GL_DEPTH_TEST)         # depth test now enabled (TP2)
+        GL.glEnable(GL.GL_CULL_FACE)          # backface culling enabled (TP2)
 
         # compile and initialize shader programs once globally
         self.color_shader = Shader(COLOR_VERT, COLOR_FRAG)
@@ -245,17 +275,26 @@ class Viewer:
         # initially empty list of object to draw
         self.drawables = []
 
-        self.color = (0, 1, 0)
+        # initialize trackball
+        self.trackball = GLFWTrackball(self.win)
 
+        # cyclic iterator to easily toggle polygon rendering modes
+        self.fill_modes = cycle([GL.GL_LINE, GL.GL_POINT, GL.GL_FILL])
+        
     def run(self):
         """ Main render loop for this OpenGL window """
         while not glfw.window_should_close(self.win):
-            # clear draw buffer
-            GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+            # clear draw buffer and depth buffer (<-TP2)
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
+            winsize = glfw.get_window_size(self.win)
+            view = self.trackball.view_matrix()
+            projection = self.trackball.projection_matrix(winsize)
+            
             # draw our scene objects
             for drawable in self.drawables:
-                drawable.draw(None, None, None, self.color_shader)
+                drawable.draw(projection, view, identity(),
+                              color_shader=self.color_shader, win=self.win)
 
             # flush render commands, and swap draw buffers
             glfw.swap_buffers(self.win)
@@ -272,32 +311,9 @@ class Viewer:
         if action == glfw.PRESS or action == glfw.REPEAT:
             if key == glfw.KEY_ESCAPE or key == glfw.KEY_Q:
                 glfw.set_window_should_close(self.win, True)
-            if key == glfw.KEY_R:
-                self.color = (1, 0, 0)
-            if key == glfw.KEY_G:
-                self.color = (0, 1, 0)
-            if key == glfw.KEY_B:
-                self.color = (0, 0, 1)
+            if key == glfw.KEY_W:
+                GL.glPolygonMode(GL.GL_FRONT_AND_BACK, next(self.fill_modes))
 
-# -------------- 3D ressource loader -----------------------------------------
-def load(file):
-    """ load resources from file using pyassimp, return list of ColorMesh """
-    try:
-        print("check 1")
-        option = pyassimp.postprocess.aiProcessPreset_TargetRealtime_MaxQuality
-        print("check 2")
-        scene = pyassimp.load(file, option)
-        print("check 3")
-    except pyassimp.errors.AssimpError:
-        print('ERROR: pyassimp unable to load', file)
-        return []     # error reading => return empty list
-
-    meshes = [ColorMesh([m.vertices, m.normals], m.faces) for m in scene.meshes]
-    size = sum((mesh.faces.shape[0] for mesh in scene.meshes))
-    print('Loaded %s\t(%d meshes, %d faces)' % (file, len(scene.meshes), size))
-
-    pyassimp.release(scene)
-    return meshes
 
 # -------------- main program and scene setup --------------------------------
 def main():
@@ -305,14 +321,11 @@ def main():
     viewer = Viewer()
 
     # place instances of our basic objects
-    #viewer.add(SimpleTriangle())
-    #viewer.add(Pyramid())
-    #viewer.add(Pyramid2())
-    #load("./resources/cylinder.obj")
-    for cmesh in load("./resources/suzanne.obj"):
-        print("Adding mesh")
-        viewer.add(cmesh)
-        print("Mesh added")
+    viewer.add(*[mesh for file in sys.argv[1:] for mesh in load(file)])
+    if len(sys.argv) < 2:
+        print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
+              ' format supported by pyassimp.' % (sys.argv[0],))
+
     # start rendering loop
     viewer.run()
 
